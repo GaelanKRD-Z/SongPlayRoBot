6d64417f1c73d5c49810068659bd2bbbec872b1f
GaelanKRD-Z <gaelankrd@outlook.com>
2024-08-24 21:27:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "21211216"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "7404d8663463075dfd564027f32810fc" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "6019956632:AAFHRc_PRuD8FFTfDBP4AJefb0zqnabOJt4" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
